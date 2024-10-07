1524c16947b74c1b57f2c35cf1044c25390c5a4f
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-10-07 07:24:38 +0300

    add firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:learnhub/home/presentation/components/home_greeting_message_ui.dart';
+import 'package:learnhub/firebase_options.dart';
+import 'package:learnhub/home/presentation/components/home_get_started_ui.dart';
 
 
-void main() {
-  runApp(const HomeGreetingMessageUi());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(const HomeGetStartedUi());
 }
 
 
