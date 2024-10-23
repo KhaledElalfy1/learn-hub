cbba240d80a2664ac4ab6fd49d30e76eb42f9d1b
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-10-23 15:58:03 +0300

    add image for courses

--- lib/features/courses/presentation/data/course_model.dart
+++ lib/features/courses/presentation/data/course_model.dart
@@ -4,8 +4,10 @@ class CourseModel {
   final String description;
   final String price;
   final String duration;
+  final String url;
 
-  CourseModel({required this.title, required this.instructor, required this.description, required this.price, required this.duration});
+  CourseModel({required this.title, required this.instructor, required this.description, required this.price, required this.duration, required this.url});
+  
 
 
 }
